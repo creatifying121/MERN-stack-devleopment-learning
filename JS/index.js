8adc3205c1d5c2e59586bb47c9231753fb62e8fc
creatifying121 <shivani.raichandani121@gmail.com>
2023-09-03 21:19:58 +0530

    basics of javascript, hoisiting, functions, variables, shortest javascript program.

---
+++ JS/index.js
@@ -0,0 +1,68 @@
+// how javascript works and how code is executed in javascript
+// var n =2; 
+// function square (num){
+//     var ans = num*num;
+//     return ans;
+// }
+// var square2 = square(n);
+// var square4 = square(4);
+
+
+// hoisting examples
+// var x = 7;
+
+// function getName() {
+//     console.log("Hey Shivani");
+// }
+
+// console.log(getName);
+// console.log(x);
+// getName();
+
+// console.log(x);
+// getName();
+
+// var x = 7;
+
+// function getName() {
+//     console.log("Hey Shivani");
+// }
+
+// console.log(x);
+// getName();
+
+
+// working of functions in javascript
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
+
+
+// window object and this keyword
+// var a = 10;
+
+// console.log(window.a); // 10
+// console.log(a); // 10
+
+// function b() {
+//     var x = 100; 
+//     console.log(x);
+// }
+
+// console.log(x); // x is not defined - because it is in function scope
+
+// console.log(b.x); // undefined
+
+
+// undefined vs. not defined
